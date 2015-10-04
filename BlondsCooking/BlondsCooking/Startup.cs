a1328b2e7b2e1f0bf8abe67c697708222ee4fdd4
agnieszkajach <agnieszka.jach@outlook.com>
2015-10-04 16:10:52 +0200

    Added empty project

---
+++ BlondsCooking/BlondsCooking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlondsCooking.Startup))]
+namespace BlondsCooking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
