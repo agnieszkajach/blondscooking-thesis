156fb81f9cf9995278490d9a45421cf87e79d273
agnieszkajach <agnieszka.jach@outlook.com>
2015-10-24 17:57:36 +0200

    Added view for single recipe and redirecting to single recipe when clicking on image

---
+++ BlondsCooking/BlondsCooking/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlondsCooking.Models.Db;
+using BlondsCooking.Models.Structure;
+
+namespace BlondsCooking.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private BlondsCookingContext context;
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Recipe(int id = 1)
+        {
+            context = new BlondsCookingContext();
+            Recipe model = context.Recipes.FirstOrDefault(recipe => recipe.Id == id);
+            return View(model);
+        }
+    }
+}
