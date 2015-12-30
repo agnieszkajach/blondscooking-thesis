156427c727fbedd50b086117aa7c4105994d7021
agnieszkajach <agnieszka.jach@outlook.com>
2015-12-30 20:18:38 +0100

    Reseting password with mail confirmation

--- BlondsCooking/BlondsCooking/App_Start/IdentityConfig.cs
+++ BlondsCooking/BlondsCooking/App_Start/IdentityConfig.cs
@@ -100,7 +100,10 @@ namespace BlondsCooking
             if (dataProtectionProvider != null)
             {
                 manager.UserTokenProvider = 
-                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"))
+                    {
+                        TokenLifespan = TimeSpan.FromHours(3)
+                    };
             }
             return manager;
         }
