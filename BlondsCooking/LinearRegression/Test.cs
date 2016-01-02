6e51fc4773a19d33609e60aa4b22853256a9a590
agnieszkajach <agnieszka.jach@outlook.com>
2016-01-02 19:43:55 +0100

    simple linear regression

---
+++ BlondsCooking/LinearRegression/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Statistics.Models.Regression.Linear;
+
+namespace LinearRegression
+{
+    public class Test
+    {
+        public void TestMethod()
+        {
+            double[] inputs = { 80, 60, 10, 20, 30 };
+            double[] outputs = { 20, 40, 30, 50, 60 };
+            SimpleLinearRegression simpleLinearRegression = new SimpleLinearRegression();
+            simpleLinearRegression.Regress(inputs, outputs);
+
+            double y = simpleLinearRegression.Compute(85);
+
+            double a = simpleLinearRegression.Slope;
+            double b = simpleLinearRegression.Intercept;
+        }
+    }
+}
