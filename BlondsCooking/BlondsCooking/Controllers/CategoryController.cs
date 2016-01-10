0b23a03c7964f575fdecb832e950b03704b24ccd
agnieszkajach <agnieszka.jach@outlook.com>
2016-01-10 16:31:16 +0100

    Basic rating of recipe

--- BlondsCooking/BlondsCooking/Controllers/CategoryController.cs
+++ BlondsCooking/BlondsCooking/Controllers/CategoryController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Web;
 using System.Web.Mvc;
+using BlondsCooking.Models;
 using BlondsCooking.Models.Db;
 using BlondsCooking.Models.Structure;
 
@@ -11,6 +13,7 @@ namespace BlondsCooking.Controllers
     public class CategoryController : Controller
     {
         private BlondsCookingContext context;
+        private RecipeRateViewModel model;
         // GET: Category
         public ActionResult Index()
         {
@@ -20,12 +23,38 @@ namespace BlondsCooking.Controllers
         public ActionResult Recipe(int id = 1)
         {
             context = new BlondsCookingContext();
-            Recipe model = context.Recipes.FirstOrDefault(recipe => recipe.Id == id);
-            if (model == null)
+            Recipe recipeModel = context.Recipes.FirstOrDefault(recipe => recipe.Id == id);
+            if (recipeModel == null)
             {
                 return View("Error");
             }
+            model = new RecipeRateViewModel()
+            {
+                Id = recipeModel.Id,
+                Name = recipeModel.Name,
+                Description = recipeModel.Description,
+                Image = recipeModel.Image,
+                Ingredients = recipeModel.Ingredients,
+                Temperature = recipeModel.Temperature,
+                Time = recipeModel.Time
+            };
             return View(model);
+        }
+
+        public void Rate(int id = 0, int rate = 0)
+        {
+            if (id != 0)
+            {
+                using (BlondsCookingContext context = new BlondsCookingContext())
+                {
+                    context.UserRatings.Add(new UserRating()
+                    {
+                        RecipeId = id,
+                        Rate = rate,
+                    });
+                    context.SaveChanges();
+                } 
+            }
         }
     }
 }
---
+++ BlondsCooking/BlondsCooking/Models/RecipeRateViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlondsCooking.Models.Structure;
+
+namespace BlondsCooking.Models
+{
+    public class RecipeRateViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string Description { get; set; }
+
+        public string Ingredients { get; set; }
+
+        public string Temperature { get; set; }
+
+        public string Time { get; set; }
+
+        public int? Rate { get; set; }
+
+        public SelectList RatesList { get; set; }
+
+        public RecipeRateViewModel()
+        {
+            List<int> rates = new List<int> { 0, 1, 2, 3, 4, 5 };
+            RatesList = new SelectList(rates);
+        }
+
+    }
+}
